7d1bb218c915392383f88c4da7117f3f5b73eec1
Rick Navarro <73374566+NavarroErk@users.noreply.github.com>
2022-12-09 00:29:59 -0800

    create functions for getting computer choice and playing a single round of rock paper scissors

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,1 +1,30 @@
-console.log("Hello World!");
+
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"]
+    let randomChoice = choices[Math.floor(Math.random() * choices.length)]
+    return randomChoice
+}
+getComputerChoice()
+
+function singleRound(playerSelection, computerSelection) {
+    computerSelection = getComputerChoice().toLowerCase()
+    if (playerSelection == computerSelection) {
+        console.log("It's a tie");
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        console.log("You lose. Paper beats rock.");
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        console.log("You win. Scissors beats paper.");
+    }else if (playerSelection == "scissors" && computerSelection == "rock") {
+        console.log("You lose. Rock beats scissors.");
+    }else if (playerSelection == "paper" && computerSelection == "rock") {
+        console.log("You win. Paper beats rock.");
+    }else if (playerSelection == "rock" && computerSelection == "scissors") {
+        console.log("You win. Rock beats scissors.");
+    }else if (playerSelection == "paper" && computerSelection == "scissors") {
+        console.log("You lose. Scissors beats paper.");
+    }
+}
+let playerSelection = prompt("Please choose rock, paper, or scissors")
+playerSelection = playerSelection.toLowerCase()
+singleRound(playerSelection, getComputerChoice())

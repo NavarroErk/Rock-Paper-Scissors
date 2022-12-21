0690bf5e45c1b129f0e0fa8b4ee65be448bc96e0
Rick Navarro <73374566+NavarroErk@users.noreply.github.com>
2022-12-20 16:26:30 -0800

    add more css styling to added HTML elements, create function that will verify player and computer scores and declare a winner when one reaches a score of 5 points, and add button that when clicked will refresh the webpage to allow the user to play again

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,93 +1,109 @@
-
-
+let playerScore = 0;
+let computerScore = 0;
+let firstTo5 = document.getElementById("firstTo5")
+let directionPara = document.getElementById("direction")
+let btnSelection = document.getElementsByClassName("btnSelection")
+let rock = document.getElementById("rockSelection");
+    rock.addEventListener("click", () => {
+    playRound("rock", getComputerChoice)
+    })
+let paper = document.getElementById("paperSelection");
+    paper.addEventListener("click", () => {
+    playRound("paper", getComputerChoice)
+    })
+let scissors = document.querySelector("#scissorsSelection");
+    scissors.addEventListener("click", () => {
+    playRound("scissors", getComputerChoice)
+})
+let result
+let paraOutput = document.getElementById("output")
+let playerScorePara = document.getElementById("playerScorePara")
+let computerScorePara = document.getElementById("computerScorePara")
+let playAgainBtn = document.getElementById("playAgainBtn")
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"]
     let randomChoice = choices[Math.floor(Math.random() * choices.length)]
     return randomChoice
 }
 getComputerChoice()
-// let playerSelection = prompt("Please choose rock, paper, or scissors")
-let playerSelection 
-// playerSelection = playerSelection.toLowerCase()
-
-let rock = document.getElementById("rockSelection");
-    rock.addEventListener("click", () => {
-    playRound("rock", getComputerChoice)
-    })
-
-let paper = document.getElementById("paperSelection");
-    paper.addEventListener("click", () => {
-    playRound("paper", getComputerChoice)
-    })
-
-let scissors = document.querySelector("#scissorsSelection");
-    scissors.addEventListener("click", () => {
-    playRound("scissors", getComputerChoice)
-})
 
 function playRound(playerSelection, computerSelection) {
     computerSelection = getComputerChoice()
-    let result
-    let outputDiv = document.getElementById("outputDiv")
-    let para = document.createElement("p")
-    para.setAttribute("id", "output")
-    outputDiv.appendChild(para)
-    let paraOutput = document.getElementById("output")
+    
     if (playerSelection == computerSelection) {
         result = `It's a tie. You both selected ${playerSelection}`
         paraOutput.textContent = result
+        verifyScore(playerScore, computerScore)
         return result
     } else if (playerSelection == "rock" && computerSelection == "paper") {
         result = `You lose. ${computerSelection} beats ${playerSelection}`
         paraOutput.textContent = result
+        computerScore++
+        computerScorePara.textContent = `Computer Score: ${computerScore}`
+        verifyScore(playerScore, computerScore)
         return result
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
         result = `You win. ${playerSelection} beats ${computerSelection}`
         paraOutput.textContent = result
+        playerScore++
+        playerScorePara.textContent = `Player Score: ${playerScore}`
+        verifyScore(playerScore, computerScore)
         return result
     }else if (playerSelection == "scissors" && computerSelection == "rock") {
         result = `You lose. ${computerSelection} beats ${playerSelection}`
         paraOutput.textContent = result
+        computerScore++
+        computerScorePara.textContent = `Computer Score: ${computerScore}`
+        verifyScore(playerScore, computerScore)
         return result
     }else if (playerSelection == "paper" && computerSelection == "rock") {
         result = `You win. ${playerSelection} beats ${computerSelection}`
         paraOutput.textContent = result
+        playerScore++
+        playerScorePara.textContent = `Player Score: ${playerScore}`
+        verifyScore(playerScore, computerScore)
         return result
     }else if (playerSelection == "rock" && computerSelection == "scissors") {
         result = `You win. ${playerSelection} beats ${computerSelection}`
         paraOutput.textContent = result
+        playerScore++
+        playerScorePara.textContent = `Player Score: ${playerScore}`
+        verifyScore(playerScore, computerScore)
         return result
     }else if (playerSelection == "paper" && computerSelection == "scissors") {
         result = `You lose. ${computerSelection} beats ${playerSelection}`
         paraOutput.textContent = result
+        computerScore++
+        computerScorePara.textContent = `Computer Score: ${computerScore}`
+        verifyScore(playerScore, computerScore)
         return result
     }
 }
 
 // console.log(playRound(playerSelection, getComputerChoice()))
 
-/* 
-let playerScore = 0;
-let computerScore = 0;
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let result = playRound() 
-        if (result == "You win") {
-            playerScore++;
-        } else if (result == "You lose") {
-            computerScore++;
-        } else if (result == "It's a tie"){
-            
-        }
-    }
-    if (playerScore > computerScore) {
-        console.log(`You win with a score of ${playerScore}. The loser has a score of ${computerScore}.`);
-    } else if (playerScore < computerScore) {
-        console.log(`You lose with a score of ${playerScore}. The winner has a score of ${computerScore}.`);
-    } else if (playerScore == computerScore) {
-        console.log(`You tie with a score of ${playerScore}.`);
+function endScreen(){
+    directionPara.style.display = "none"
+        firstTo5.style.display = "none"
+        rock.disabled = true
+        paper.disabled = true
+        scissors.disabled = true
+        paraOutput.style.fontSize = "40pt"
+        playAgainBtn.style.display = ""
+}
+
+function verifyScore(playerScore, computerScore) {
+    if (playerScore == 5) {
+        endScreen()
+        paraOutput.textContent = "Congratulations, you win!"
+        paraOutput.style.color = "green"
+    } else if (computerScore == 5) {
+        endScreen()
+        paraOutput.textContent = "You lose. Better luck next time!"
+        paraOutput.style.color = "red"
     }
 }
 
-game() 
-*/
+playAgainBtn.addEventListener("click", () => location.reload())
+
+
